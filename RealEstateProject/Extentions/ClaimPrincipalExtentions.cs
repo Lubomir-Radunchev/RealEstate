01f7693b9b0a7f51d15e00044106ffb5e6987224
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-06-09 15:20:32 +0300

    GetId can be nullble here

--- RealEstateProject/Extentions/ClaimPrincipalExtentions.cs
+++ RealEstateProject/Extentions/ClaimPrincipalExtentions.cs
@@ -4,9 +4,9 @@ namespace RealEstateProject.Extentions
 {
     public static class ClaimPrincipalExtentions
     {
-        public static string GetId(this ClaimsPrincipal user)
-                        => user.FindFirst(ClaimTypes.NameIdentifier).Value;
-        
+        public static string? GetId(this ClaimsPrincipal user)
+                        => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
         //public static bool IsAdmin(this ClaimsPrincipal user)
         //   => user.IsInRole(Admin);
     }
