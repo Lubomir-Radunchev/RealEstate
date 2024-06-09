e7bb814355ca27c52a33834c489a39ce11ef1525
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-06-09 15:18:16 +0300

    DealerService was created

---
+++ RealEstateProject/Services/DealerService.cs
@@ -0,0 +1,18 @@
+﻿using RealEstateProject.Database;
+using RealEstateProject.Database.Models;
+
+namespace RealEstateProject.Services
+{
+    public class DealerService : IDealerService
+    {
+        private readonly ApplicationDbContext data;
+
+        public DealerService(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        public Dealer? GetByUserId(string userId)
+          => this.data.Dealers.FirstOrDefault(x => x.IdentityUserId == userId);
+    }
+}
---
+++ RealEstateProject/Services/IDealerService.cs
@@ -0,0 +1,10 @@
+﻿using RealEstateProject.Database.Models;
+
+namespace RealEstateProject.Services
+{
+    public interface IDealerService
+    {
+        // nullable return 
+        Dealer? GetByUserId(string userId);
+    }
+}
