df26462c7d02539d99716716e85e60041552c6e5
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-07-09 19:24:29 +0300

    Dto model fixed

--- RealEstateProject/DtosModel/HouseDto/HouseFormDto.cs
+++ RealEstateProject/DtosModel/HouseDto/HouseFormDto.cs
@@ -1,18 +1,31 @@
 ﻿using RealEstateProject.Database.Models;
+using RealEstateProject.Database.Models.Enums;
 using System.Diagnostics.CodeAnalysis;
 
 namespace RealEstateProject.DtosModel.HouseDto
 {
     public class HouseFormDto
     {
-       
         public string Street { get; set; }
-      
+
         public string City { get; set; }
 
-       
+        public decimal Price { get; set; }
+
+        public decimal? RentPrice { get; set; }
+
+
         public int Quadrature { get; set; }
+        public string Picture { get; set; }
+
+        public string Description { get; set; }
+        public int DealerId { get; set; }
+        public Dealer Dealer { get; set; }
         public Condition Condition { get; set; }
-        public byte[] Picture { get; set; }
+
+        public List<string> Conditions = new List<string>();
+        public UseType UseType { get; set; }
+        public List<string> Types = new List<string>();
     }
+
 }
