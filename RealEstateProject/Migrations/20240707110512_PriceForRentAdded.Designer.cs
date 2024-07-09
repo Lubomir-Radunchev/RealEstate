497cb15e07a9e7d4041f6674b0753a0c50e5066a
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-07-09 19:24:50 +0300

    Migrations were created

--- RealEstateProject/Migrations/20240707110512_PriceForRentAdded.Designer.cs
+++ RealEstateProject/Migrations/20240707110512_PriceForRentAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealEstateProject.Database;
 
@@ -11,9 +12,11 @@ using RealEstateProject.Database;
 namespace RealEstateProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240707110512_PriceForRentAdded")]
+    partial class PriceForRentAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RealEstateProject/Migrations/20240707110512_PriceForRentAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceForRentAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "RentPrice",
+                table: "Houses",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentPrice",
+                table: "Houses");
+        }
+    }
+}
